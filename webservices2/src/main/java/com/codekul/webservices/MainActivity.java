a823b52ab1e4d09613a1f9c2913ed19f13a99e8c
aniruddha <anniruddha.kudalkar@gmail.com>
2016-10-19 13:00:04 +0530

    webservice post and custom request

--- webservices2/src/main/java/com/codekul/webservices/MainActivity.java
+++ webservices2/src/main/java/com/codekul/webservices/MainActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        findViewById(R.id.btnCall).setOnClickListener(this::callGet);
+        findViewById(R.id.btnCall).setOnClickListener(this::callPostGson);
     }
 
     private void callGet(View view) {
@@ -43,5 +50,59 @@ public class MainActivity extends AppCompatActivity {
 
     private void volleyError(VolleyError volleyError) {
         Log.i("@codekul"," Volly error - "+volleyError.toString());
+    }
+
+    private void callPost(View view) {
+        JSONObject dataToSend = new JSONObject();
+        try {
+            dataToSend.put("title","android");
+            dataToSend.put("body","codekul");
+            dataToSend.put("userId",100);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Ws.q(this)
+                .add(new JsonObjectRequest(Request.Method.POST,
+                        "http://jsonplaceholder.typicode.com/posts",
+                        dataToSend,
+                        this::onResponse,
+                        this::onError));
+    }
+
+    private void onError(VolleyError volleyError) {
+        Log.i("@codekul","Response Error - "+volleyError.toString());
+    }
+
+    private void onResponse(JSONObject jsonObject) {
+        Log.i("@codekul","Response Json - "+jsonObject.toString());
+    }
+
+    private void onStringResponse(String jsonResponse) {
+    }
+
+    private void callPostGson(View view){
+        Gson gson = new Gson();
+        MyReq req = new MyReq();
+        req.setBody("Codekul");
+        req.setTitle("android");
+        req.setUserId(100l);
+
+        Ws.q(this).add(new JsonRequest<MyReq>(Request.Method.POST,"http://jsonplaceholder.typicode.com/posts",gson.toJson(req),res->{
+            Log.i("@codekul","Body - "+res.getBody());
+        },err->{
+
+        }) {
+            @Override
+            protected Response<MyReq> parseNetworkResponse(NetworkResponse response) {
+                Log.i("@codekul","Post Gson - "+new String(response.data));
+                return Response.success(gson.fromJson(new String(response.data),MyReq.class),null);
+            }
+
+            @Override
+            protected VolleyError parseNetworkError(VolleyError volleyError) {
+                return super.parseNetworkError(volleyError);
+            }
+        });
     }
 }
---
+++ webservices2/src/main/java/com/codekul/webservices/MyReq.java
@@ -0,0 +1,36 @@
+package com.codekul.webservices;
+
+/**
+ * Created by aniruddha on 19/10/16.
+ */
+
+public class MyReq {
+
+    private String title;
+    private String body;
+    private Long userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
